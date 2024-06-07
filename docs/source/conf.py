449290ea921eed9f28cdb903d07961e16996fecd
emgbench <emgbench@gmail.com>
2024-06-07 18:31:53 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EMG Benchmarking'
+copyright = '2024, Jehan Yang'
+author = 'Jehan Yang'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
